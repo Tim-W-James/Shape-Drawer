a54d13a0743ea44525e4b4baadaf72b6e9f90225
timja <u6947396@anu.edu.au>
2019-03-27 00:49:50 +1100

    PartB - Task 3: all events defined and are functional

--- src/Controller.hs
+++ src/Controller.hs
@@ -21,16 +21,33 @@ handleEvent event m@(Model ss t c) =
       -- display the mystery image
       | k == "M" -> Model mystery t c
 
-      | k == "Backspace" || k == "Delete" -> undefined -- TODO: drop the last added shape
-      | k == " " -> undefined -- TODO: finish polygon vertices
-      | k == "T" -> undefined -- TODO: switch tool
-      | k == "C" -> undefined -- TODO: switch colour
+      | k == "Backspace" || k == "Delete" -> case ss of -- drop the last added shape
+        [] -> Model ss t c
+        _ -> Model (tail ss) t c
+      | k == " " -> case t of -- finish polygon vertices
+        PolygonTool pointList
+          | (length pointList) < 3 -> Model ss t c -- pointList must have at least 3 members
+          | otherwise -> Model ((c,(Polygon pointList)):ss) (PolygonTool []) c
+        _ -> Model ss t c
+      | k == "T" -> Model ss (nextTool t) c -- switch tool
+      | k == "C" -> Model ss t (nextColour c) -- switch colour
 
       -- ignore other events
       | otherwise -> m
       where k = unpack key
-    PointerPress p -> undefined -- TODO
-    PointerRelease p -> undefined -- TODO
+    PointerPress p -> case t of
+      PolygonTool pointList             -> Model ss (PolygonTool (p : pointList)) c
+      LineTool _                        -> Model ss (LineTool (Just p)) c
+      RectangleTool _                   -> Model ss (RectangleTool (Just p)) c
+      CircleTool _                      -> Model ss (CircleTool (Just p)) c
+      EllipseTool _                     -> Model ss (EllipseTool (Just p)) c
+    PointerRelease p -> case t of
+      PolygonTool _                     -> Model ss t c
+      LineTool (Just firstPoint)        -> Model ((c,(Line firstPoint p)):ss) (LineTool Nothing) c
+      RectangleTool (Just firstPoint)   -> Model ((c,(Rectangle firstPoint p)):ss) (RectangleTool Nothing) c
+      CircleTool (Just firstPoint)      -> Model ((c,(Circle firstPoint p)):ss) (CircleTool Nothing) c
+      EllipseTool (Just firstPoint)     -> Model ((c,(Ellipse firstPoint p)):ss) (EllipseTool Nothing) c
+      _                                 -> Model ss t c
     _ -> m
 
 -- | Returns the next colour from the set of ColourNames.
